0593f51c4ff6e5a2d853d46fc8983dd56f096823
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-12 18:33:48 +0100

    upgrade main module version

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "remote-state-s3-backend" {
-  source                  = "nozaq/remote-state-s3-backend/aws"
-  version                 = "1.4.0"
-  dynamodb_table_name     = var.tf_state_lock_table_name
-  override_s3_bucket_name = true
-  s3_bucket_name          = var.tf_state_s3_bucket_name
-  enable_replication      = false
-
+  source                      = "nozaq/remote-state-s3-backend/aws"
+  version                     = "1.5.0"
+  dynamodb_table_name         = var.tf_state_lock_table_name
+  override_s3_bucket_name     = true
+  s3_bucket_name              = var.tf_state_s3_bucket_name
+  enable_replication          = false
+  terraform_iam_policy_create = false
   tags = {
     ManagedBy = "Terraform"
   }
